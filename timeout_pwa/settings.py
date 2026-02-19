1ad55b3b732dd2e91194fa31044f9fe7959f0a25
JUYEOP <80294898+sheep06031@users.noreply.github.com>
2026-02-19 15:42:33 +0000

    Update ALLOWED_HOSTS to allow all hosts

--- timeout_pwa/settings.py
+++ timeout_pwa/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production-use-environment-variable
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
