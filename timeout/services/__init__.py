87239f34a80e9ffa9a7bb9d7d5e90a718000261a
Ozan Kaya <hikmetozankaya@gmail.com>
2026-03-10 00:04:10 +0000

    redesign landing page, complete dark mode fixes, create settings, implement pamadora, improve notes page

--- timeout/services/__init__.py
+++ timeout/services/__init__.py
@@ -1,4 +1,5 @@
 from .feed_service import FeedService
 from .note_service import NoteService
+from .deadline_service import DeadlineService
 
 __all__ = ['FeedService', 'NoteService', 'DeadlineService']
