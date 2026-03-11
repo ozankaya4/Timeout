06c9988095f70140a1f2ce6c9ef5ac53955df437
Erin_Ozdemir <erin.ozdemir0@gmail.com>
2026-03-11 16:38:41 +0000

    merge conflicts fixed

--- timeout/views/calendar.py
+++ timeout/views/calendar.py
@@ -196,11 +196,8 @@ def calendar_view(request):
         "next_year": next_year,
         "next_month": next_month,
         "weekdays": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-<<<<<<< HEAD
         "workload_warning": workload_warning,
-=======
         "reschedule_prompts": reschedule_prompts,
->>>>>>> origin
     }
 
     return render(request, "pages/calendar.html", context)
